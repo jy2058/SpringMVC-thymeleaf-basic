f5425b83549bf3fcac0b3c62877429da78ba3a65
jykim <jiy3285@gmail.com>
2022-10-07 22:18:47 +0900

    URL 링크

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -66,6 +66,14 @@ public class BasicController {
         return "basic/date";
     }
 
+    @GetMapping("/link")
+    public String link(Model model){
+        model.addAttribute("param1", "data1");
+        model.addAttribute("param2", "data2");
+
+        return "basic/link";
+    }
+
     @Data
     private class User {
         private String username;
